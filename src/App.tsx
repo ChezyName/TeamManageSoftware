8d71bede532d482f94e2998be65a741f6b236073
ChezyName <mineboy2468@gmail.com>
2023-03-13 00:56:14 -0500

    Top Menu Bar Finished

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="root">
-      <div className="TopBar" style={{width: '100%', height: '42px',display: 'flex' , backgroundColor: 'var(--foreground)'}}>
+      <div className="TopBar" style={{width: '100%', height: '42px',display: 'flex' , backgroundColor: 'var(--foreground)', borderBottom: '0px solid var(--border)'}}>
         <Button variant="danger"><FontAwesomeIcon icon={faRotateLeft}/> RESET</Button>
-
-        <Button variant="primary"><FontAwesomeIcon icon={faAdd}/> CREATE NEW</Button>
+        <span style={{margin: 'auto',flex: '1', width: '60%', textAlign: 'center', color: 'white', fontSize: '1.25rem', fontWeight: "bold",userSelect: 'none'}}>Team Manage Software</span>
+        <Button variant="primary" style={{marginLeft: 'auto'}}>CREATE NEW <FontAwesomeIcon icon={faAdd}/> </Button>
       </div>
     </div>
   );
