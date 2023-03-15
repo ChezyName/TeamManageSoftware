b2a51d886cc1413685f16629152ffd3cf6421a92
ChezyName <mineboy2468@gmail.com>
2023-03-14 20:36:03 -0500

    Added Rows edit Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { WebviewWindow } from '@tauri-apps/api/window'
 import { BaseDirectory,writeTextFile,exists, createDir, readTextFile} from '@tauri-apps/api/fs';
@@ -11,18 +11,25 @@ import Table from 'react-bootstrap/Table';
 import CreateNew from "./CreateNew";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRotateLeft, faAdd } from "@fortawesome/free-solid-svg-icons";
+import { faRotateLeft, faAdd, faPencil } from "@fortawesome/free-solid-svg-icons";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css'
+
+interface DataType {
+  name: string;
+  ids: string[];
+  works: boolean[];
+}
 
 function App() {
   const [isCreate, setCreate] = useState(false);
-  const [data,setData] = useState("");
+  const [data,setData] = useState<DataType[]>([]);
+
+  const saveFileLoc = "save.json";
 
   async function Create(){
-    const saveFileLoc = "save.json";
     let dir = await exists("",{ dir: BaseDirectory.AppData});
-    setData(dir ? "TRUE" : "FALSE");
     if(!dir) await createDir("",{ dir: BaseDirectory.AppData})
 
 
@@ -31,20 +38,37 @@ function App() {
       await writeTextFile(saveFileLoc, '',{ dir: BaseDirectory.AppData});
     }
     else{
-      let data = await readTextFile(saveFileLoc,{ dir: BaseDirectory.AppData});
+      let cdata = await readTextFile(saveFileLoc,{ dir: BaseDirectory.AppData});
+      setData(JSON.parse(cdata));
     }
   }
 
-  Create();
+  function Save(){
+    writeTextFile(saveFileLoc,JSON.stringify(data),{ dir: BaseDirectory.AppData})
+  }
+
+  useEffect(() => {
+    Create();
+  }, [])
+  
 
   return (
     <div className="root">
       {isCreate ? <CreateNew onClickFunc={(value:string) => {
         console.log("Creating {"+value+"}");
         if(value != "") {
-          
+          for(var i = 0; i < data.length; i++){
+            let d = data[i];
+            if(d.name == value){
+              return;
+            }
+          }
+
+          let newData = data;
+          newData.push({name: value, ids: [], works: []});
+          setData(newData);
         }
-        Create();
+        Save();
         setCreate(false);
       }}/> : ""}
       <div className="TopBar" style={{width: '100%', height: '42px',display: 'flex' , backgroundColor: 'var(--foreground)', borderBottom: '0px solid var(--border)'}}>
@@ -63,24 +87,37 @@ function App() {
             <th colSpan={10}>Parts Number</th>
             <th>% Completed</th>
             <th>% Failed</th>
+            <th><FontAwesomeIcon icon={faPencil}/> Edit</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>{data}</td>
-            <td>N/A</td>
-            <td>N/A</td>
-            <td>N/A</td>
-            <td>N/A</td>
-            <td>N/A</td>
-            <td>N/A</td>
-            <td>N/A</td>
-            <td>N/A</td>
-            <td>N/A</td>
-            <td>N/A</td>
-            <td>0%</td>
-            <td>0%</td>
-          </tr>
+          {
+            data.map((dta:DataType) => {
+              let name = dta.name;
+              let singles:any = [];
+              let percent_works = 0;
+              for(var i = 0; i < 10; i++){
+                let id = dta.ids[i];
+                let works = dta.works[i];
+                if(works) percent_works++;
+
+                singles.push(
+                  <th style={{color: works ? "green" : 'red'}}>{id == null ? "N/A" : id}</th>
+                )
+              }
+              return <tr>
+                <th>{name}</th>
+                {
+                  singles
+                }
+                <th>{percent_works}%</th>
+                <th>{percent_works}%</th>
+                <button onClick={() => {
+                  console.log("On Clicked:");
+                }} className="editButton" style={{border: '0',width: '100%'}}><FontAwesomeIcon style={{color: 'white !important'}} icon={faPencil}/></button>
+              </tr>
+            })
+          }
         </tbody>
       </Table>
     </div>
